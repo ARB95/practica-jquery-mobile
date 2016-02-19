685539a4bbe8d61877129373043cac234e94669e
ARB95 <albertorus1995@hotmail.com>
2016-02-20 00:59:15 +0100

    Sistema de cambio de tema
    Sistema de cambio de tema dinámico iniciado y acabado

---
+++ js/temas.js
@@ -0,0 +1,23 @@
+function cambiarTema(tema)
+{
+	//alert(tema);
+	limpiarTemas();
+	if(tema != "a" && tema != "b" && tema != "c" && tema != "d") tema = "a";
+	$("body, body > div").addClass( "ui-page-theme-" + tema );
+	localStorage.setItem("tema",tema);
+}
+
+function limpiarTemas()
+{
+	$("body, body > div").removeClass("ui-page-theme-a ui-page-theme-b ui-page-theme-c ui-page-theme-d");
+	$("body, body > div").removeClass("ui-overlay-a");
+}
+
+$(document).ready(function() {
+	cambiarTema(localStorage.getItem("tema"));
+	
+	$(".cambiaTema").click(function(event){
+		
+		cambiarTema($(this).attr("value"));
+	});
+});
